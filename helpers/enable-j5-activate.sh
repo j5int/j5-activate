4cb4d897ef1955e4faa82caf7c021f364b3661db
David Fraser <davidf@j5int.com>
2017-09-06 09:19:00 +0200

    Better error handling, and more reliable directory identification

--- helpers/enable-j5-activate.sh
+++ helpers/enable-j5-activate.sh
@@ -14,11 +14,12 @@ j5activate() {
         J5DIR=$J5_PARENT_GIT_DIR/j5-framework/j5/src/
     elif [ "${1#-}" != "$1" ]; then
         echo syntax j5activate "[framework-src-label]"
+        return 1
     else
         J5DIR=$J5_PARENT_GIT_DIR/j5-framework-$1/j5/src/
-        [ -d "$J5DIR" ] || { echo Could not locate j5 framework - $J5DIR does not exist >&2 ; return ; }
+        [ -d "$J5DIR" ] || { echo Could not locate j5 framework - $J5DIR does not exist >&2 ; return 1 ; }
     fi
-    [ -f "$J5DIR/Scripts/j5activate.sh" ] || { echo The j5 framework version in $J5DIR does not support j5activate >&2 ; return ; }
+    [ -f "$J5DIR/Scripts/j5activate.sh" ] || { echo The j5 framework version in $J5DIR does not support j5activate >&2 ; return 1 ; }
     source "$J5DIR/Scripts/j5activate.sh"
 }
 
--- helpers/include.sh
+++ helpers/include.sh
@@ -1,6 +1,8 @@
 # Helper functions
 
-J5_ACTIVATE_HELPERS_DIR="`dirname "$BASH_SOURCE"`"
+_include_script="`readlink -f "$BASH_SOURCE"`"
+J5_ACTIVATE_HELPERS_DIR="`dirname "$_include_script"`"
 J5_ACTIVATE_DIR="`dirname "$J5_ACTIVATE_HELPERS_DIR"`"
+J5_ACTIVATE_BIN_DIR="$J5_ACTIVATE_DIR/bin"
 J5_PARENT_GIT_DIR="`dirname "$J5_ACTIVATE_DIR"`"
 
