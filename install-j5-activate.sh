f9f8077249dc3083664d2e9e65d1cad3d6ae60e6
Matt Hampton <matth@j5int.com>
2017-09-07 18:16:56 +0200

    Get the j5-activate shell scripts to work on windows in bash (for git). And run the bash script, too (with git-bash)

--- install-j5-activate.sh
+++ install-j5-activate.sh
@@ -4,8 +4,15 @@ _this_script="`readlink -f "$BASH_SOURCE"`"
 . "`dirname "$_this_script"`/helpers/include.sh"
 . "$J5_ACTIVATE_HELPERS_DIR/colored-echo.sh"
 
+if [[ "`uname`" == MINGW6* ]] ;
+then
+    #Git-bash on windows (MINGW doesn't look in .bashrc - only .bash_profile
+    bashrc="$HOME/.bash_profile"
+else
+    bashrc="$HOME/.bashrc"
+fi
+
 bashrc_changed=
-bashrc="$HOME/.bashrc"
 bash_aliases_file="$J5_ACTIVATE_HELPERS_DIR/j5_bash_aliases.sh"
 grep "^source '$bash_aliases_file'" "$bashrc" > /dev/null || {
     colored_echo blue "Setting up j5 bash aliases"
@@ -14,6 +21,6 @@ grep "^source '$bash_aliases_file'" "$bashrc" > /dev/null || {
 }
 duplicate_check=`grep -c "j5_bash_aliases.sh" "$bashrc"`
 [ "$duplicate_check" != "1" ] && colored_echo red "Check of $bashrc found $duplicate_check references to j5_bash_aliases.sh - should be 1. Check this file"
-[ "$bashrc_changed" != "" ] && colored_echo blue "You will need to source .bashrc (restart bash) for new aliases"
+[ "$bashrc_changed" != "" ] && colored_echo blue "You will need to source .bashrc / .bash_profile on Windows (or restart bash) for new aliases"
 [ "$duplicate_check" != "1" ] && exit 1
 
