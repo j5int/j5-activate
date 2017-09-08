5761ae57ded99438470f9e619117e0be395986d5
Matt Hampton <matth@j5int.com>
2017-09-08 04:38:15 +0200

    Implement the j5activate script for windows - this calls the appropriate activate script based on the parameters.

--- bin/j5activate.cmd
+++ bin/j5activate.cmd
@@ -1,2 +1,20 @@
 @echo off
-REM call SOME j5 activate script
+if "%1"=="-?" goto usage
+if "%1"=="/?" goto usage
+if "%1"=="-h" goto usage
+if "%1"=="/h" goto usage
+
+if [%1]==[] goto nolabel
+
+call "%~dp0\..\..\j5-framework-%1\j5\src\Scripts\helpers\_j5activate.cmd"
+
+goto :eof
+
+:nolabel
+
+call "%~dp0\..\..\j5-framework\j5\src\Scripts\helpers\_j5activate.cmd"
+
+goto :eof
+
+:usage
+@echo syntax j5activate [framework-src-label]
