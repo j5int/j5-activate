9890bb3fcec6ba259a68fbfbf12024e48990a6b7
Matt Hampton <matth@j5int.com>
2017-09-08 04:45:41 +0200

    I renamed _j5activate.cmd to j5activate.cmd so it doesn't look funny to call it from the build scripts.

--- bin/j5activate.cmd
+++ bin/j5activate.cmd
@@ -6,13 +6,13 @@ if "%1"=="/h" goto usage
 
 if [%1]==[] goto nolabel
 
-call "%~dp0\..\..\j5-framework-%1\j5\src\Scripts\helpers\_j5activate.cmd"
+call "%~dp0\..\..\j5-framework-%1\j5\src\Scripts\helpers\j5activate.cmd"
 
 goto :eof
 
 :nolabel
 
-call "%~dp0\..\..\j5-framework\j5\src\Scripts\helpers\_j5activate.cmd"
+call "%~dp0\..\..\j5-framework\j5\src\Scripts\helpers\j5activate.cmd"
 
 goto :eof
 
