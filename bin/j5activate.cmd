200a3d9a7fa0012f07ee5ba952bf11a5ba073b2d
David Fraser <davidf@j5int.com>
2020-01-27 20:31:04 +0200

    Handle --python3 and a worktree version label
    This requires using a temporary variable rather than ~dp0 as shift seems to adjust that, for fun
    The alternative of proper command-line parameter handling ends up being too ghastly

--- bin/j5activate.cmd
+++ bin/j5activate.cmd
@@ -1,22 +1,27 @@
 @echo off
+set activate_args=
+set "activate_path=%~dp0"
 if "%1"=="-?" goto usage
 if "%1"=="/?" goto usage
 if "%1"=="-h" goto usage
 if "%1"=="/h" goto usage
-if "%1"=="--python3" goto nolabel
+if "%1"=="--python3" (
+    set activate_args=--python3
+    shift
+)
 
 if [%1]==[] goto nolabel
 
-set "src_dir=%~dp0\..\..\j5-framework-%1"
+set "src_dir=%activate_path%\..\..\j5-framework-%1"
 goto :activate
 
 :nolabel
 
-set "src_dir=%~dp0\..\..\j5-framework"
+set "src_dir=%activate_path%\..\..\j5-framework"
 if exist "%src_dir%" goto :activate
 
 set "tmpfile=%tmp%\j5path_%RANDOM%.txt"
-powershell -NoProfile -ExecutionPolicy unrestricted -File "%~dp0\find_j5_src.ps1" > "%tmpfile%"
+powershell -NoProfile -ExecutionPolicy unrestricted -File "%activate_path%\find_j5_src.ps1" > "%tmpfile%"
 set failure=%errorlevel%
 if %failure% neq 0 (
   type "%tmpfile%" >&2
@@ -25,21 +30,29 @@ if %failure% neq 0 (
 )
 set /p src_dir=<"%tmpfile%"
 del "%tmpfile%"
+set tmpfile=
 echo Found source directory at %src_dir%
 
 goto :activate
 
 :activate
-call "%src_dir%\j5\src\Scripts\helpers\j5activate.cmd" %*
+call "%src_dir%\j5\src\Scripts\helpers\j5activate.cmd" %activate_args%
 if %ERRORLEVEL% GEQ 1 goto :error
 set src_dir=
-goto :eof
+goto :end
 
 :usage
-@echo syntax j5activate [framework-src-label] [--python3]
-goto :eof
+@echo syntax j5activate [--python3] [framework-src-label]
+goto :end
 
 :error
 set src_dir=
 echo j5activate failed >&2
+set activate_path=
+set activate_args=
 exit /b 1
+
+:end
+rem clearing variables
+set activate_path=
+set activate_args=
